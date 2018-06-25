3e5c86cc2716aa30c370a9c86d1c915254dfa213
replicanti-galaxies <40543740+replicanti-galaxies@users.noreply.github.com>
2018-06-25 21:09:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,17 +10,19 @@ var matterOnPrestige = 0;
 var cprice = 1000;
 
 function updateHTML(){
-a("ramount").innerHTML = "you have " + replicanti + " replicanti";
-a("chance").innerHTML = "replicate chance: " + chance + "%";
-a("tickspeed").innerHTML = "tickspeed: " + tickspeed + "ms";
-a("amount").innerHTML = "you have " + matter + " matter (+" + matterOnPrestige +")";
-a("cprice").innerHTML = cprice + " matter to upgrade chance";
-a("updaterate").innerHTML = "update rate: " + updateRate + "ms"
+	console.log("UPDATE")
+	a("ramount").innerHTML = "you have " + replicanti + " replicanti";
+	a("chance").innerHTML = "replicate chance: " + chance + "%";
+	a("tickspeed").innerHTML = "tickspeed: " + tickspeed + "ms";
+	a("amount").innerHTML = "you have " + matter + " matter (+" + matterOnPrestige +")";
+	a("cprice").innerHTML = cprice + " matter to upgrade chance";
+	a("updaterate").innerHTML = "update rate: " + updateRate + "ms"
 
-if(!matter){a("reduction").innerHTML = "0% reduction to tickspeed"} else {a("reduction").innerHTML = tickspeed/matter + "% reduction to tickspeed"}
+	if(!matter){a("reduction").innerHTML = "0% reduction to tickspeed"} else {a("reduction").innerHTML = tickspeed/matter + "% reduction to tickspeed"}
 }
 
 var slider = a("updateRange")
+
 slider.oninput = function() {
   updateRate = slider.value;
   clearInterval(updateLoop)
@@ -28,19 +30,20 @@ slider.oninput = function() {
 }
 
 function updateReplicanti(){
-if(replicanti < 100){
-  for(i=0;i<replicanti;i++){
-	 if(chance > Math.random()*100){
-	  replicanti++;
-  updateHTML()
-	 }
-  }
-} else {
-	let x = 0;
-	for(i=0;i<100;i++){
-if(chance > Math.random()*100){
-	x++
-} 
+	console.log("updating")
+	if(replicanti < 100){
+	  for(i=0;i<replicanti;i++){
+		 if(chance > Math.random()*100){
+		  replicanti++;
+	  updateHTML()
+		 }
+	  }
+	} else {
+		let x = 0;
+		for(i=0;i<100;i++){
+	if(chance > Math.random()*100){
+		x++
+	} 
 }
 	replicanti = Math.floor(replicanti*(1+x/100));
 updateHTML()
@@ -48,4 +51,4 @@ updateHTML()
 }
 
 updateLoop = window.setInterval(updateHTML(), updateRate)
-replicantiLoop = window.setInterval(updateReplicanti(), tickspeed)
+replicantiLoop = window.setInterval(updateReplicanti(), 1000)
